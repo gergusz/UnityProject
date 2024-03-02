4089628d471836d577efd17fe2d4cde9f4b4a1c1
bastyha <szentesidominik@gmail.com>
2024-03-02 19:34:34 +0100

    background movement option 1

---
+++ Assets/Scripts/BgMove.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgMove : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject characterToMove=null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void MoveChar()
+    {
+
+        Vector3 vel_of_character= characterToMove.GetComponent<Rigidbody2D>().velocity;
+        transform.Translate(vel_of_character * Time.deltaTime, Camera.main.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterToMove == null)
+            transform.Translate(Vector3.right * Time.deltaTime, Camera.main.transform);
+        else
+            MoveChar();
+
+    }
+}
