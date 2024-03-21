b0ae23e7703e3278b8f81a0074df5f991f6f8589
gergusz <gergusz03@proton.me>
2024-03-21 23:45:28 +0100

    movement + multi jump
    poggers

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,21 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Movement : MonoBehaviour
 {
-    Rigidbody2D _rigidbody2D;
-    Vector2 _moveVel;
-    float _moveDir;
-    [SerializeField] private float moveSpeed = 233f;
-    [SerializeField] private float jumpForce = 7f;
-    private bool _jumpPressed;
-    private bool _isGrounded;
+
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private int maxJumps = 1;
+    private Rigidbody2D _rb;
+    private bool _facingLeft;
+    private bool _onGround;
+    private int _jumps;
+    private float _horizontalAxis;
+    private bool _jumpButtonHeldDown;
+    private bool _canJump = true;
 
     private void Awake()
     {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
+    
 
     // Start is called before the first frame update
     private void Start()
@@ -27,44 +33,65 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        GetInput();
+        _horizontalAxis = Input.GetAxis("Horizontal");
+        _jumpButtonHeldDown = Input.GetButton("Jump");
     }
-
-    private void GetInput()
-    {
-        _moveDir = Input.GetAxisRaw("Horizontal");
-        _jumpPressed |= Input.GetButtonDown("Jump");
-    }
-
-    private void Move()
-    {
-        _moveVel = _rigidbody2D.velocity;
-        _moveVel.x = _moveDir * moveSpeed * Time.fixedDeltaTime;
-        _rigidbody2D.velocity = _moveVel;
-    }
-
-    private void HandleJump()
-    {
-        if (_jumpPressed && _isGrounded)
-        {
-            _rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            _jumpPressed = false;
-        }
-    }   
 
     private void FixedUpdate()
     {
-        Move();
-        HandleJump();
+        var currVelocity = _rb.velocity;
+        currVelocity.x = _horizontalAxis * speed;
+        _rb.velocity = currVelocity;
+
+        if (_horizontalAxis < 0 && !_facingLeft)
+        {
+            Flip();
+        }
+        else if (_horizontalAxis > 0 && _facingLeft)
+        {
+            Flip();
+        }
+
+        if (_jumpButtonHeldDown && _canJump)
+        {
+            TryJump();
+            _canJump = false;
+        }
+
+        if (!_jumpButtonHeldDown || _onGround)
+        {
+            _canJump = true;
+        }
+
+    }
+
+    private void TryJump()
+    {
+        if (_onGround) 
+        {
+            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            _onGround = false;
+            _jumps++;
+        } 
+        else if (_jumps < maxJumps)
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, 0);
+            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            _jumps++;
+        }
+    }
+
+    private void Flip()
+    {
+        _facingLeft = !_facingLeft;
+        var theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        _isGrounded = true;
-    }
-
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        _isGrounded = false;
+        _onGround = true;
+        _jumps = 0;
     }
 }
